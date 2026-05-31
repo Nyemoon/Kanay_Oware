844f7c1f572e0453e11a1e106ab99f4ddca4a686
Ketlyn Barbosa <kbl1@aluno.ifal.edu.br>
2026-05-31 10:00:20 -0300

    refactor: simplificar lógica e remover redundâncias

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/tokens.css';
 import App from './App';
-import './MancalaGame.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
